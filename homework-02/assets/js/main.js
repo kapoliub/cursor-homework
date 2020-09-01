6434675f01bf2a95f8cb474140c311b6680bcf6d
Liubomyr <louidropson@gmail.com>
2020-09-01 15:33:13 +0300

    little fixes

--- homework-02/assets/js/main.js
+++ homework-02/assets/js/main.js
@@ -1,34 +1,36 @@
-let sum = 0;
-let N = Math.floor(+prompt('Enter number #1 (empty field = 0)'));
-console.log(N)
+function getSumOfArray(){
+    let sum = 0;
+    let N = Math.floor(+prompt('Enter number #1 (empty field = 0)'));
+    // console.log(N)
 
-while(isNaN(N)){
-    N = +prompt('Enter number #1');
-}
+    while(isNaN(N)){
+        N = +prompt('Enter number #1');
+    }
 
-let M = Math.floor(+prompt('Enter number #2 (bigger than #1)'));
+    let M = Math.floor(+prompt('Enter number #2 (bigger than #1)'));
 
-while(isNaN(M) || M <= N){
-    M = +prompt('Enter number #2 (bigger than #1)');
-}
+    while(isNaN(M) || M < N){
+        M = +prompt('Enter number #2 (bigger than #1)');
+    }
 
-let skipEvenNumbers = confirm('Skip even numbers?\n "Cancel" - no \n "Ok" - yes')
+    let skipEvenNumbers = confirm('Skip even numbers?\n "Cancel" - no \n "Ok" - yes')
 
-if(skipEvenNumbers){
-    for(let i = N; i <= M; i++){
-        if(i % 2 !== 0){
+    if(skipEvenNumbers){
+        for(let i = N; i <= M; i++){
+            if(i % 2 !== 0){
+                sum += i;
+            }
+        }
+        return sum
+    }
+    else{
+        for(let i = N; i <= M; i++){
             sum += i;
         }
-    }
-
-}
-else{
-    for(let i = N; i <= M; i++){
-        sum += i;
+        return sum
     }
 }
 
-console.log(sum)
-
+console.log(getSumOfArray())
 
 
